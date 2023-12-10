b374b6951abb7c35c7f1dd022e28e42e338271f2
RK <gorkvilar@gmail.com>
2023-12-10 21:08:29 +0100

    First Commit

---
+++ src/content/posts/03-loled.md
@@ -0,0 +1,9 @@
+---
+title: "Untitled2"
+publishedAt: 2023-05-24
+description: "Lorem ipsum dolor sit amet consectetur et ultrices blandit neque ege"
+slug: "lul"
+isPublish: true
+---
+
+## Hello World
---
+++ src/data/presentation.ts
@@ -0,0 +1,33 @@
+type Social = {
+  label: string;
+  link: string;
+};
+
+type Presentation = {
+  mail: string;
+  title: string;
+  description: string;
+  socials: Social[];
+  profile?: string;
+};
+
+const presentation: Presentation = {
+  mail: "gorka@iand.dev",
+  title: "Hi, I’m Gorka 👋",
+  // profile: "/profile.webp",
+  description:
+    "Hola, i'm a *andorran developer* with over *1 year* of web experience. I am currently working with *Python, NextJS and Typescript*. Interested on how AI can help us to improve our lives. Learning about *Machine Learning* and *Data Science*.",
+  socials: [
+
+    {
+      label: "Github",
+      link: "https://github.com/GRKdev",
+    },
+    {
+      label: "LinkedIn",
+      link: "https://www.linkedin.com/in/gorka-vilar/",
+    },
+  ],
+};
+
+export default presentation;
