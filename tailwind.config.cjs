5e68198d64c2deb4df4d1cc54320baf91ef4ff7a
RK <gorkvilar@gmail.com>
2023-12-17 18:54:03 +0100

    Update profile image and add animation to title

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,16 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        tilt: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(10deg)' },
+          '75%': { transform: 'rotate(-10deg)' },
+        },
+      },
+      animation: {
+        tilt: 'tilt 0.5s ease-in-out 3',
+      },
     },
   },
   plugins: [],
