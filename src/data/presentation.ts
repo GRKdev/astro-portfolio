bee870fbf8d9ff09ae0f0f77d808af585d4ea68c
RK <gorkvilar@gmail.com>
2023-12-26 21:50:58 +0100

    Update profile image in presentation.ts

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -14,7 +14,7 @@ type Presentation = {
 const presentation: Presentation = {
   mail: "gorka@iand.dev",
   title: "*Hi*, I’m Gorka ",
-  profile: "/12.jpeg",
+  profile: "/profile-logo.webp",
   description:
     "Andorran *developer* currently working with *Python, NextJS and Typescript*. Interested on how *AI* can help us to improve our lives. Learning about *Machine Learning* and *Data Science*.",
 
